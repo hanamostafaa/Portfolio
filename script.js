23a16f5570b48dd832c2a2657f021e145c8f47b6
hanamostafaa <hmostafa935@gmail.com>
2023-08-22 00:52:54 +0300

    new file:   images/Ellipse 27 --_ Ellipse 28.svg 	deleted:    images/ay haga.png 	deleted:    images/cert1.jfif 	deleted:    images/cert2.jfif 	new file:   images/computer.jpg 	new file:   images/curly.jpg 	deleted:    images/icons8-javascript.svg 	new file:   images/linkedin-svgrepo-com (2).svg 	modified:   images/logo.svg 	new file:   images/open.svg 	deleted:    images/photo.png 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let slideIndex = 0;
-showSlides();
 const el = document.querySelector(".nav-bar")
 const observer = new IntersectionObserver( 
   ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
@@ -7,15 +5,58 @@ const observer = new IntersectionObserver(
 );
 
 observer.observe(el);
+const all=document.querySelector("#project-button-1");
+const oop=document.querySelector("#project-button-2");
+const web=document.querySelector("#project-button-3");
+const robotics=document.querySelector("#project-button-4");
+const logic=document.querySelector("#project-button-5");
+const dsa=document.querySelector("#project-button-6");
+const projectsArr=document.querySelectorAll("#project-item");
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+const displayOop=()=>
+{
+    for(const project of projectsArr )
+    {
+        project.style.display='none';
+    }
+    projectsArr[0].style.display='block';
+    console.log("test");
+
 }
+const display=(x)=>
+{
+    for(const project of projectsArr )
+    {
+        project.style.display='none';
+    };
+    if(x===-1)
+    {
+        for(const project of projectsArr )
+        {
+            project.style.display='block';
+        };
+    }
+    else if(x===0)
+    projectsArr[0].style.display='block';
+    else if(x===1)
+    projectsArr[1].style.display='block';
+    else if(x===2)
+    projectsArr[2].style.display='block';
+    else if(x===3)
+    {
+    projectsArr[3].style.display='block';
+    projectsArr[4].style.display='block';
+    }
+    else if(x===4)
+    {
+    projectsArr[5].style.display='block';
+    }
+
+}
+all.addEventListener("click",()=>display(-1));
+oop.addEventListener("click",()=>display(0));
+web.addEventListener("click",()=>display(1));
+robotics.addEventListener("click",()=>display(2));
+logic.addEventListener("click",()=>display(3));
+dsa.addEventListener("click",()=>display(4));
