41e091ef281ff90d80cfc9e52f64a0fa014c18f7
Mariam Amin <128838373+Mariam-Amin12@users.noreply.github.com>
2023-08-23 22:53:24 +0300

    last one

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 const el = document.querySelector(".nav-bar")
-const observer = new IntersectionObserver( 
-  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
-  { threshold: [1] }
+const observer = new IntersectionObserver(
+    ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
+    { threshold: [1] }
 );
 
+
 observer.observe(el);
-const all=document.querySelector("#project-button-1");
-const oop=document.querySelector("#project-button-2");
-const web=document.querySelector("#project-button-3");
-const robotics=document.querySelector("#project-button-4");
-const logic=document.querySelector("#project-button-5");
-const dsa=document.querySelector("#project-button-6");
-const projectsArr=document.querySelectorAll("#project-item");
+const all = document.querySelector("#project-button-1");
+const oop = document.querySelector("#project-button-2");
+const web = document.querySelector("#project-button-3");
+const robotics = document.querySelector("#project-button-4");
+const logic = document.querySelector("#project-button-5");
+const dsa = document.querySelector("#project-button-6");
+const projectsArr = document.querySelectorAll("#project-item");
 
 
-const displayOop=()=>
-{
-    for(const project of projectsArr )
-    {
-        project.style.display='none';
+const displayOop = () => {
+    for (const project of projectsArr) {
+        project.style.display = 'none';
     }
-    projectsArr[0].style.display='block';
+    projectsArr[0].style.display = 'block';
     console.log("test");
 
 }
-const display=(x)=>
-{
-    for(const project of projectsArr )
-    {
-        project.style.display='none';
+const display = (x) => {
+    for (const project of projectsArr) {
+        project.style.display = 'none';
     };
     all.classList.remove("yellow-line");
     web.classList.remove("yellow-line");
@@ -36,45 +33,132 @@ const display=(x)=>
     dsa.classList.remove("yellow-line");
     logic.classList.remove("yellow-line");
     robotics.classList.remove("yellow-line");
-    if(x===-1)
-    {
-        for(const project of projectsArr )
-        {
-            project.style.display='block';
+    if (x === -1) {
+        for (const project of projectsArr) {
+            project.style.display = 'block';
         };
         all.classList.add("yellow-line");
     }
-    else if(x===0)
-    {
-    projectsArr[0].style.display='block';
-    oop.classList.add("yellow-line");
+    else if (x === 0) {
+        projectsArr[0].style.display = 'block';
+        oop.classList.add("yellow-line");
     }
-    else if(x===1)
-    {
-    projectsArr[1].style.display='block';
-    web.classList.add("yellow-line");
+    else if (x === 1) {
+        projectsArr[1].style.display = 'block';
+        web.classList.add("yellow-line");
     }
-    else if(x===2)
-    {
-    projectsArr[2].style.display='block';
-    robotics.classList.add("yellow-line");
+    else if (x === 2) {
+        projectsArr[2].style.display = 'block';
+        robotics.classList.add("yellow-line");
     }
-    else if(x===3)
-    {
-    projectsArr[3].style.display='block';
-    projectsArr[4].style.display='block';
-    logic.classList.add("yellow-line");
+    else if (x === 3) {
+        projectsArr[3].style.display = 'block';
+        projectsArr[4].style.display = 'block';
+        logic.classList.add("yellow-line");
     }
-    else if(x===4)
-    {
-    projectsArr[5].style.display='block';
-    dsa.classList.add("yellow-line");
+    else if (x === 4) {
+        projectsArr[5].style.display = 'block';
+        dsa.classList.add("yellow-line");
     }
 
 }
-all.addEventListener("click",()=>display(-1));
-oop.addEventListener("click",()=>display(0));
-web.addEventListener("click",()=>display(1));
-robotics.addEventListener("click",()=>display(2));
-logic.addEventListener("click",()=>display(3));
-dsa.addEventListener("click",()=>display(4));
+all.addEventListener("click", () => display(-1));
+oop.addEventListener("click", () => display(0));
+web.addEventListener("click", () => display(1));
+robotics.addEventListener("click", () => display(2));
+logic.addEventListener("click", () => display(3));
+dsa.addEventListener("click", () => display(4));
+
+
+
+
+const Home = document.querySelector(".Home");
+const About = document.querySelector(".About");
+const Certification = document.querySelector(".Certifi");
+const Projects = document.querySelector(".Project");
+const Contact = document.querySelector(".Contact");
+const navbuttons = document.querySelectorAll(".button")
+
+// console.log(navbuttons);
+const addyellowline = (x) => {
+    for (const button of navbuttons) {
+        button.classList.remove("yellow-line2");
+    }
+    if (x == 1) {
+        Home.classList.add("yellow-line2");
+
+    }
+    else if (x == 2) {
+        About.classList.add("yellow-line2");
+    }
+    else if (x == 3) {
+        Certification.classList.add("yellow-line2");
+    }
+    else if (x == 4) {
+        Projects.classList.add("yellow-line2");
+    }
+    else {
+        Contact.classList.add("yellow-line2");
+    }
+}
+Home.addEventListener(("click"), () => addyellowline(1));
+About.addEventListener(("click"), () => addyellowline(2));
+Certification.addEventListener(("click"), () => addyellowline(3));
+Projects.addEventListener(("click"), () => addyellowline(4));
+Contact.addEventListener(("click"), () => addyellowline(5));
+
+
+const isView = (section) => {
+    const rect = section.getBoundingClientRect();
+    return (
+        rect.top >= window.innerHeight - 1700 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+
+}
+
+window.addEventListener(('scroll'), () => {
+    const sectionarr = document.querySelectorAll('.page-Section')
+
+    sectionarr.forEach((ele, ind) => {
+        if (isView(ele))
+            addyellowline(ind + 1);
+        console.log(isView(ele));
+
+    })
+
+
+})
+const switchbutton = document.querySelector('.circle')
+
+
+
+
+
+
+
+switchbutton.addEventListener(('click'), () => {
+    const style = document.documentElement.style;
+    if (style.getPropertyValue('--background') === '#ffffff') {
+        style.setProperty('--background', '#000000');
+        style.setProperty('--font', '#ffffff');
+        style.setProperty('--shadow', '#fff200');
+        style.setProperty('--nav-background', "#FFB800");
+        style.setProperty('--line', "#ffffff");
+        style.setProperty('--switch', "#FFB800");
+
+
+    } else {
+        style.setProperty('--background', '#ffffff');
+        style.setProperty('--font', '#000000');
+        style.setProperty('--shadow', '#FFB800');
+        style.setProperty('--nav-background', "#cbcbcb80");
+        style.setProperty('--line', "#FFB800");
+        style.setProperty('--switch', "#000000");
+
+    }
+
+}
+)
+
+
